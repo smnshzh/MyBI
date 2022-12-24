3277270a1078dabd3d2ced006499a1cfdfe38d08
Asus <smnshzh@gmail.com>
2022-12-24 20:34:10 +0330

    Bsic Models is completing

--- basicData/admin.py
+++ basicData/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 from .models import *
 
 class AdminMain(admin.ModelAdmin):
@@ -14,6 +15,13 @@ admin.site.register(GoodsMainGroup,AdminMain)
 admin.site.register(GoodsSubGroup,AdminSub)
 admin.site.register(GoodsDivideGroup,AdminDivide)
 admin.site.register(Good,AdminGood)
+admin.site.register(Dc)
+admin.site.register(Barache)
+admin.site.register(DistrictManager)
+admin.site.register(Manager)
+admin.site.register(SaleLine)
+admin.site.register(CostumersActivity)
+
 
 
 
--- basicData/models.py
+++ basicData/models.py
@@ -58,8 +58,88 @@ class Dc(models.Model):
 
     code= models.IntegerField(unique=True)  
     name = models.CharField(max_length=225)
-    groups = models.ManyToManyField(to=GoodsMainGroup)
-    lines = models.ManyToManyField(to=GoodsDivideGroup)
+    ggroups = models.ManyToManyField(to=GoodsMainGroup,related_name="GoodsCanSAle")
+    activation = models.BooleanField(default=False)
     class Meta:
-        verbose_name = "مرکز اصلی"
-        verbose_name_plural = "مراکز اصلی"
+        verbose_name = "5-مرکز اصلی"
+        verbose_name_plural = "5-مراکز اصلی"
+    def __str__(self):
+        return self.name
+class Barache(models.Model):
+
+    code = models.IntegerField()
+    name = models.CharField(max_length = 225)
+    mainDc = models.ForeignKey(to=Dc,on_delete=models.DO_NOTHING)
+    activation = models.BooleanField(default=False)
+    class Meta:
+        verbose_name = "6-مرکز فروش"
+        verbose_name_plural = "6-مراکز فروش"
+    def __str__(self):
+        return self.name
+
+
+class DistrictManager(models.Model):
+    code = models.IntegerField()
+    name = models.CharField(max_length=225)
+    activation = models.BooleanField(default=False)
+    MainDcs = models.ManyToManyField(Dc,related_name="MainDC")
+
+    class Meta:
+        verbose_name= "7-مدیر منطقه"
+        verbose_name_plural = "7-مدیران مناطق"
+    def __str__(self):
+        return self.name
+CHOISE = [
+    (1,"مدیر"),
+    (2,"رئیس")
+]
+class Manager(models.Model):
+    code = models.IntegerField()
+    name = models.CharField(max_length=225)
+    activation = models.BooleanField(default=False)
+    MainDc = models.ForeignKey(Dc,on_delete=models.DO_NOTHING)
+    position = models.CharField(max_length=4,choices=CHOISE)
+    class Meta:
+        verbose_name="8-مدیر شعبه"
+        verbose_name_plural = "8-مدیریت شعب"
+    def __str__(self):
+        return f"{self.position} {self.name}"
+class Superviseur(models.Model):
+    code = models.IntegerField()
+    name = models.CharField(max_length=225)
+    activation = models.BooleanField(default=False)
+    MainDc = models.ForeignKey(Dc, on_delete=models.DO_NOTHING)
+    class Meta:
+        verbose_name = "9-سرپرست"
+        verbose_name_plural = "9-سرپرستان"
+
+    def __str__(self):
+        return self.name
+
+class SaleLine(models.Model):
+
+    code = models.IntegerField()
+    name = models.CharField(max_length=225)
+    divideGroup = models.ManyToManyField(GoodsDivideGroup)
+    class Meta:
+        verbose_name = "10-لاین فروش"
+        verbose_name_plural = "10-لاین های فروش"
+    def __str__(self):
+        return self.name
+
+class CostumersActivity(models.Model):
+    code = models.IntegerField()
+    name = models.CharField(max_length=225)
+    class Meta:
+        verbose_name = "11-فعالیت مشتری"
+        verbose_name_plural = f"11-فعالیتها"
+    def __str__(self):
+        return self.name
+
+class SalerActivity(models.Model):
+    code = models.IntegerField()
+    name = models.CharField(max_length=225)
+    costumerActivity = models.ManyToManyField(CostumersActivity)
+
+    class Meta:
+        verbose_name = ""
